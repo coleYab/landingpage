1d5b6b747b0feeefb935c869c3e27ed91148c1b2
coleYab <yabume123@gmail.com>
2025-11-17 11:12:06 +0300

    fix: added some thing

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import Wrapper from "@/layouts/wrapper";
 import HomeThree from "@/components/homes/home-3";
 
 export const metadata = {
-  title: "Softec - Data analytics"
+  title: "Axonova Consulting",
 };
 
 const HomePage = () => {
