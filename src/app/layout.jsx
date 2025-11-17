6c4c16032b9f350b5e5f9112ec4843086826161d
coleYab <yabume123@gmail.com>
2025-11-17 13:02:22 +0300

    feat: add search engine optimization

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,6 +6,8 @@ import {
   Urbanist,
 } from "next/font/google";
 
+import Script from "next/script";
+
 const dmSerifDisplay = DM_Serif_Display({
   weight: ["400"],
   subsets: ["latin"],
@@ -42,6 +44,21 @@ export default function RootLayout({ children }) {
       <body
         className={`${dmSerifDisplay.variable} ${montserratAlternates.variable} ${plusJakartaSans.variable} ${urbanist.variable}`}
       >
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-933ZPDQHR4"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-933ZPDQHR4');
+          `}
+        </Script>
+
         {children}
       </body>
     </html>
